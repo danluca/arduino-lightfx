ff174f1728f9aa7911bc70d07eaea0b077f47797
Dan Luca <danluca22@gmail.com>
2024-01-10 20:19:20 -0600

    updated build script for new env names

--- update.ps1
+++ update.ps1
@@ -1,12 +1,13 @@
 [CmdletBinding()]
-param ([ValidateSet('auto', 'com3', 'com4')][string]$port='auto')
+param ([ValidateSet('auto', 'com3', 'com4')][string]$port='auto', [switch]$debug)
 
 
 #######################################
 ## Main
 #######################################
+$profile = $debug ? "rp2040-dbg" : "rp2040-rel"
 if ($port -eq 'auto') {
-    pio run -t upload
+    pio run -t upload -e $profile
 } else {
-    pio run -t upload --port $port
+    pio run -t upload -e $profile --port $port
 }
