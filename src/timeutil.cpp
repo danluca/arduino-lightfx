3b23b0c28dab1f6b2c12be3011a816373e941b5d
Dan Luca <danluca22@gmail.com>
2024-08-05 17:58:09 -0500

    fixed log time offset units

--- src/timeutil.cpp
+++ src/timeutil.cpp
@@ -39,8 +39,12 @@ bool time_setup() {
             timeClient.setTimeOffset(CST_OFFSET_SECONDS);   //getEpochTime calls account for the offset
         setTime(timeClient.getEpochTime());    //ensure the offset change above (if it just transitioned) has taken effect
         hday = paletteFactory.adjustHoliday();    //update the holiday for new time
-        if (logTimeOffset == 0)
-            logTimeOffset = now() - millis();                //capture current time into the log offset, such that log statements use current time
+        if (logTimeOffset == 0) {
+            time_t curTime = now();
+            time_t curMs = millis();
+            Log.warningln(F("Logging time reference updated from %y (%u) to %y"), curMs, curMs, curTime);
+            logTimeOffset = curTime * 1000 - curMs;                //capture current time into the log offset, such that log statements use current time
+        }
 #ifndef DISABLE_LOGGING
         Log.infoln(F("America/Chicago %s time, time offset set to %d s, current time %s. NTP sync ok."),
                    bDST?"Daylight Savings":"Standard", bDST?CDT_OFFSET_SECONDS:CST_OFFSET_SECONDS, timeClient.getFormattedTime().c_str());
