944667cff35f224bcaad5b7288f72765db725244
Dan Luca <danluca22@gmail.com>
2025-11-20 21:09:04 -0600

    added helper shell scrips

---
+++ scripts/status.ps1
@@ -0,0 +1,21 @@
+## Copyright (c) 2025 by Dan Luca. All rights reserved.
+##
+## Build script for PlatformIO project
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("Dev","Tree")]
+    [string]$board = "Dev"
+)
+
+#######################################
+## Global
+#######################################
+$brdUri = "http://192.168.0.139"  # Default URI for Dev board
+switch ($board) {
+    "Tree" { $brdUri = "http://192.168.0.182"; break; }
+    "FX02" { $brdUri = "http://192.168.0.12"; break; }
+}
+write-host "Board $board at $brdUri - Status`n" -ForegroundColor Yellow
+
+iwr $brdUri/status.json | select -ExpandProperty Content
